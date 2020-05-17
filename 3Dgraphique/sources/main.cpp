679c16699dc874ca54ba85a326e9e78f63f7212a
minhthuc2502 <minhthuc2502@gmail.com>
2020-05-18 00:42:10 +0200

    uniform color

--- 3Dgraphique/sources/main.cpp
+++ 3Dgraphique/sources/main.cpp
@@ -1,7 +1,7 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 #include <iostream>
-
+#include <cmath>
 /**
  * Call back function when resize window
 */
@@ -34,16 +34,29 @@ int main()
 
     const char *vertexShaderSource = "#version 330 core\n"
     "layout (location = 0) in vec3 aPos;\n"
+    "out vec4 vertexColor;\n"
     "void main()\n"
     "{\n"
-    "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+    "   gl_Position = vec4(aPos, 1.0);\n"
+    "   vertexColor = vec4(0.5, 0.0, 0.0, 1.0);\n"
     "}\0";
+    /*
     const char *fragmentShaderSource = "#version 330 core\n"
     "out vec4 FragColor;\n"
+    "in vec4 vertexColor;\n"
     "void main()\n"
     "{\n"
-    "FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+    "FragColor = vertexColor;\n"
     "}\n";
+    */
+    const char *fragmentShaderSource = "#version 330 core\n"
+    "out vec4 FragColor;\n"
+    "uniform vec4 varyColor;\n"
+    "void main()\n"
+    "{\n"
+    "FragColor = varyColor;\n"
+    "}\n";
+
     unsigned int vertexShader;
     unsigned int fragmentShader;
     unsigned int shaderProgram;
@@ -173,6 +186,12 @@ int main()
         glClear(GL_COLOR_BUFFER_BIT);
         // draw object (triangle)
         glUseProgram(shaderProgram);
+        // update the uniform color
+        float timevalue = glfwGetTime();
+        float greenValue = sin(timevalue) / 2.0f + 0.5f;
+        int vertexColorLocation = glGetUniformLocation(shaderProgram, "varyColor");
+        glUniform4f(vertexColorLocation, 0.0f, greenValue, 0.0f, 1.0f);
+
         glBindVertexArray(VAO);
         //glDrawArrays(GL_TRIANGLES, 0, 3);
         glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
